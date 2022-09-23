b195bf53f0d9e50b4c8f255a8409580d5476d328
penahkerimov99 <104515090+penahkerimov99@users.noreply.github.com>
2022-09-23 20:16:14 +0400

    fvfsv

--- src/components/Footersec/Footersec.jsx
+++ src/components/Footersec/Footersec.jsx
@@ -7,10 +7,10 @@ export const Footersec = () => {
     <div className='footer-section'>
     <div className="tittle9">Copyright in 2022 PANAH KARIMOV</div>
     <div className="iconbar3">
-        <div className="icon4"><a href=""><i class="fa-brands fa-instagram"></i></a></div>
-        <div className="icon4"><a href=""><i class="fa-brands fa-facebook-f"></i></a></div>
-        <div className="icon4"><a href=""><i class="fa-brands fa-whatsapp"></i></a></div>
-        <div className="icon4"><a href=""><i class="fa-brands fa-twitter"></i></a></div>
+        <div className="icon4"><a href="/"><i class="fa-brands fa-instagram"></i></a></div>
+        <div className="icon4"><a href="/"><i class="fa-brands fa-facebook-f"></i></a></div>
+        <div className="icon4"><a href="/"><i class="fa-brands fa-whatsapp"></i></a></div>
+        <div className="icon4"><a href="/"><i class="fa-brands fa-twitter"></i></a></div>
     </div>
     </div>
   )
--- src/components/Mainnav/Mainnav.jsx
+++ src/components/Mainnav/Mainnav.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 import './style.scss'
 
 
 
 export const Mainnav = () => {
+    
     return (
         <nav id='home' className='main-nav'>
-
+               
                 <ul>
-                <li><a href='#home'  className='navitem'>Home</a></li>
-                <li><a href='#desc' className='navitem'>About</a></li>
-                <li><a href='#skill'  className='navitem'>Skils</a></li>
-                <li><a href='#contact' className='navitem'>Contact</a></li>
+                <li><AnchorLink href='#home'  className='navitem'>Home</AnchorLink></li>
+                <li><AnchorLink href='#desc' className='navitem'>About</AnchorLink></li>
+                <li><AnchorLink href='#skill'  className='navitem'>Skils</AnchorLink></li>
+                <li><AnchorLink href='#contact' className='navitem'>Contact</AnchorLink></li>
                 </ul>
-    
-
+               
         </nav>
     )
 }
